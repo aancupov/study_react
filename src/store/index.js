6be6659fc7dc663fd39883686add732afc5d7b95
andrew <aancupov@yandex.ru>
2017-12-01 22:02:59 +0500

    change for eslint

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,12 @@
-import { createStore, applyMiddleware//, compose 
-} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import thunk from 'redux-thunk';
-
-//import DevTools from 'containers/DevTools';
 
 import reducers from 'reducers';
 
 const store = createStore(
   reducers, 
-  //compose(
   applyMiddleware(thunk)
-  //  , 
-  //  DevTools.instrument()
-  //)
 );
 
 export default store;
