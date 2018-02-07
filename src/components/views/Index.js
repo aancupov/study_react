7c770cc577f9a3b3bfbc29b0f7d74d50bfe47b38
andrew <aancupov@yandex.ru>
2018-02-07 08:49:00 +0500

    refactoring

--- src/components/views/Index.js
+++ src/components/views/Index.js
@@ -6,9 +6,9 @@ import PropTypes from 'prop-types';
 
 import BlogList from 'components/ui/BlogList';
 
-const Index = ({ items, pagination }) => (
+const Index = ({ items, pagination, search }) => (
   <div>
-    <BlogList items={items} pagination={pagination}/>
+    <BlogList items={items} pagination={pagination} search={search} />
     <Helmet 
       title='Список постов'
     />
@@ -17,7 +17,8 @@ const Index = ({ items, pagination }) => (
 
 Index.propTypes = {
   items: BlogList.propTypes.items,
-  pagination: PropTypes.number
+  pagination: PropTypes.number,
+  search: PropTypes.func
 };
 
 export default Index;
